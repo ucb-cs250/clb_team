f5d6bd25b4ebe8152dfa84a8a5f928add4d0a70b
Ryan Thornton <rpthornton@berkeley.edu>
2020-12-07 10:52:40 -0800

    Added pre-decoded support for LUTs

--- src/behavioral/block_config_latches.v
+++ src/behavioral/block_config_latches.v
@@ -4,7 +4,8 @@
  */
 module block_config_latches #(
     parameter ADDR_BITS=4, 
-    parameter MEM_SIZE=2**ADDR_BITS
+    parameter MEM_SIZE=2**ADDR_BITS,
+    parameter PREDEC=0 // useful only for 4-LUT right now
 ) (
     // IO
     input [ADDR_BITS-1:0] addr, 
@@ -17,10 +18,31 @@ module block_config_latches #(
 );
 
 reg [MEM_SIZE-1:0] mem = 0;
-assign out = mem[addr];
 
+generate
+    if (PREDEC==1) begin
+        wire [MEM_SIZE/4-1:0] intermediate_out;
+        wire [MEM_SIZE/4-1:0] intermediate_use; 
+        genvar i;
+        for (i = 0; i < MEM_SIZE/4; i=i+1) begin
+            assign intermediate_out[i] = mem[{addr[ADDR_BITS-1:ADDR_BITS-2], {(ADDR_BITS-2){1'b0}}}];
+            transmission_gate tg(intermediate_out[i], out, intermediate_use[i]);
+            if (i==0) 
+                assign intermediate_use = ~addr[1] & ~addr[0];
+            else if (i==1) 
+                assign intermediate_use = ~addr[1] & addr[0];
+            else if (i==2) 
+                assign intermediate_use = addr[1] & ~addr[0];
+            else 
+                assign intermediate_use = addr[1] & addr[0];
+        end
+    end
+    else begin
+        assign out = mem[addr];
+    end
+endgenerate
 // Block Style Configuration Logic
-always @(posedge clk) begin
+always @(clk) begin
     if (comb_set) begin
         mem <= config_in;
     end
