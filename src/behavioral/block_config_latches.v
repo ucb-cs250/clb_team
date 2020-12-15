23549f6140f9b7ac2a46e3e93c766ff241674ba5
Ryan Thornton <rpthornton@berkeley.edu>
2020-12-15 12:32:59 -0800

    passes rtl, must change transmission_gate to transmission_gate_rtl for sim

--- src/behavioral/block_config_latches.v
+++ src/behavioral/block_config_latches.v
@@ -18,8 +18,6 @@ module block_config_latches #(
 );
 
 reg [MEM_SIZE-1:0] mem = 0;
-wire invout;
-assign out = ~invout;
 
 generate
     if (PREDEC==1) begin
@@ -27,16 +25,16 @@ generate
         wire [3:0] intermediate_use;
         genvar i;
         for (i = 0; i < MEM_SIZE/4; i=i+1) begin
-            mux_kareem impl(mem[4*i+3:4*i], intermediate_out[i], addr[1:0]);
-            transmission_gate tg(intermediate_out[i], invout, intermediate_use[i]);
+            mux impl(mem[4*i+3:4*i], intermediate_out[i], addr[1:0]);
+            transmission_gate tg(intermediate_out[i], out, intermediate_use[i]);
             if (i==0)
-                assign intermediate_use[i] = ~addr[1] & ~addr[0];
+                assign intermediate_use[i] = ~addr[ADDR_BITS-1] & ~addr[ADDR_BITS-2];
             else if (i==1) 
-                assign intermediate_use[i] = ~addr[1] & addr[0];
+                assign intermediate_use[i] = ~addr[ADDR_BITS-1] & addr[ADDR_BITS-2];
             else if (i==2) 
-                assign intermediate_use[i] = addr[1] & ~addr[0];
+                assign intermediate_use[i] = addr[ADDR_BITS-1] & ~addr[ADDR_BITS-2];
             else 
-                assign intermediate_use[i] = addr[1] & addr[0];
+                assign intermediate_use[i] = addr[ADDR_BITS-1] & addr[ADDR_BITS-2];
         end
     end
     else begin
